4fd9d57d2dd89d1fc0af646db16af2515b96ed29
Ilyas Safiullin <Safiullin1Ilyas@yandex.ru>
2024-09-30 20:04:02 +0300

    Binary tree
    Has and Add done

--- binaryTree.js
+++ binaryTree.js
@@ -3,73 +3,64 @@ class BinaryTree {
 
     add(item) {
         if (!this.#root) {
-            this.#root = createNode()
-            return
+            this.#root = createNode();
+            return;
         }
 
-        addNodeRec(this.#root)
+        addNodeRec(this.#root);
 
         function createNode() {
             return {
                 value: item,
                 left: null,
                 right: null,
-            }
+            };
         }
 
         function addNodeRec(node) {
-            if (node.value > item) {
+            if (item < node.value) {
+
+                if (node.left) {
+                    addNodeRec(node.left);
+                } else {
+                    node.left = createNode();
+                }
+            } else {
 
                 if (node.right) {
-                    addNodeRec(node.right)
-                }
-
-                else {
+                    addNodeRec(node.right);
+                } else {
                     node.right = createNode();
-                }
-
-            }
-            else {
-                if (node.left) {
-                    addNodeRec(node.left)
-                }
-
-                else {
-                    node.left = createNode();
                 }
             }
         }
     }
 
     has(item) {
-        return hasRec(this.#root)
+        return hasRec(this.#root);
 
         function hasRec(node) {
+            if (!node) return false;
+
             if (node.value === item) {
                 return true;
             }
 
-            else if (node.value > item) {
+            if (item < node.value) {
 
-                if (node.right) {
-                    return hasRec(node.right)
-                }
-                return false
+                return hasRec(node.left);
+            } else {
 
-            }
-            else {
-                if (node.left) {
-                    return hasRec(node.left)
-                }
-                return false
+                return hasRec(node.right);
             }
         }
     }
 }
 
-const bT = new BinaryTree()
 
-bT.add(10)
-bT.add(15)
-bT.add(32)
-console.log(bT.has(32))
+const bT = new BinaryTree();
+
+bT.add(10);
+bT.add(15);
+bT.add(32);
+
